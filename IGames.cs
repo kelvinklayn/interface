ef368fccd7a7592c6676bad0936611688ef5f27a
kelvin klayn <kelvin.klayn94@gmail.com>
2022-04-01 09:05:42 -0300

    Update IGames.cs

--- IGames.cs
+++ IGames.cs
@@ -5,18 +5,6 @@
         string GameName { get; }
         string Category { get; }
         string Price { get; }
-
-        public void Multiplayer(string option)
-        {
-            if (option == "s")
-            {
-                Console.WriteLine("Playable with Friend");
-            }
-            else
-            {
-                Console.WriteLine("Not Playable with Friend");
-            }
-        }
     }
 }
 
